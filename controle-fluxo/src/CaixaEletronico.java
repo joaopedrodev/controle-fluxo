680b600248df9448be15e3bf74b2abcc461d4ba0
Joaopedrodev <joaopedrodacostavale@gmail.com>
2023-03-16 18:46:51 -0300

    Aula de condição composta digital innovation one.

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args){
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 4.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("Seu saldo atual é : " +  " " + saldo);
+            
+        }else
+            System.out.println("Saldo insuficiente" + "Seu atual saldo e de " + saldo);    
 
-            System.out.println(saldo);
+            
     }
 }
