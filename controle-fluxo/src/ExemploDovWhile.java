924788c1953f7db3dc9d888c3985b73ee778ed71
Joaopedrodev <joaopedrodacostavale@gmail.com>
2023-03-18 11:46:22 -0300

    Aula sobre Do While pela Dio

---
+++ controle-fluxo/src/ExemploDovWhile.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+public class ExemploDovWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do{
+            //executando repetidas vezes até alguém atender
+            System.out.println("Telefone tocando");
+        }while(tocando());
+    }
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        //negando o ato de continuar tocando
+        return ! atendeu;
+    }
+}
