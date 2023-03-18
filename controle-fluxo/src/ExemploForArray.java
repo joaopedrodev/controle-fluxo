061a33508743d650796fe331aca495e40d31bc6d
Joaopedrodev <joaopedrodacostavale@gmail.com>
2023-03-18 11:02:16 -0300

    Aula de for pela Digital Innovation One

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -2,8 +2,8 @@ public class ExemploForArray {
     public static void main(String[] args) {
         String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
 
-        for (int x=0; x < alunos.length; x++){
-            System.out.println("Oaluno no indice x=" + x + " é " + alunos[x]);
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
         }
     }
 }
